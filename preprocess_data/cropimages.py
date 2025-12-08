3b24aaa82c036082cd6a3f267fab13c731c723c5
hwcho511 <kenshin511@naver.com>
2025-12-08 16:13:09 +0900

    fix: 데이터 증강 코드 수정; 파일 경로 등

--- preprocess_data/cropimages.py
+++ preprocess_data/cropimages.py
@@ -8,10 +8,11 @@ def makedir(path):
         os.makedirs(path)
 
 # set paths
-rootpath = ''
-imgspath = rootpath + ''
-trainpath = rootpath + ''
-testpath = rootpath + ''
+rootpath = '/nas/datasets/CUB_200_2011/CUB_200_2011/'
+imgspath = rootpath + '/images/'
+croppath = rootpath + 'cub200_cropped/images/'
+trainpath = rootpath + 'cub200_cropped/train_cropped/'
+testpath = rootpath + 'cub200_cropped/test_cropped/'
 
 # read img names, bounding_boxes
 names = pd.read_table(rootpath + 'images.txt', delimiter=' ', names=['id', 'name'])
@@ -19,17 +20,21 @@ names = names.to_numpy()
 boxs = pd.read_table(rootpath + 'bounding_boxes.txt', delimiter=' ',names=['id', 'x', 'y', 'width', 'height'])
 boxs = boxs.to_numpy()
 
+folders = pd.read_table(rootpath + 'classes.txt', delimiter=' ', names=['id', 'folder'])
+folders = folders.to_numpy()
+
+for i in range(200):
+    makedir(croppath + folders[i][1])
+
 # crop imgs
 for i in range(11788):
     im = Image.open(imgspath + names[i][1])
     im = im.crop((boxs[i][1], boxs[i][2], boxs[i][1] + boxs[i][3], boxs[i][2] + boxs[i][4]))
-    im.save(imgspath + names[i][1], quality=95)
+    im.save(croppath + names[i][1], quality=95)
     print('{} imgs cropped and saved.'.format(i + 1))
 print('All Done.')
 
 # mkdir for cropped imgs
-folders = pd.read_table(rootpath + 'classes.txt', delimiter=' ', names=['id', 'folder'])
-folders = folders.to_numpy()
 for i in range(200):
     makedir(trainpath + folders[i][1]) #200
     makedir(testpath + folders[i][1])
@@ -39,8 +44,8 @@ labels = pd.read_table(rootpath + 'train_test_split.txt', delimiter=' ', names=[
 labels = labels.to_numpy()
 for i in range(11788):
     if(labels[i][1] == 1):
-        copyfile(imgspath + names[i][1], trainpath + names[i][1])
+        copyfile(croppath + names[i][1], trainpath + names[i][1])
     else:
-        copyfile(imgspath + names[i][1], testpath + names[i][1])
+        copyfile(croppath + names[i][1], testpath + names[i][1])
     print('{} imgs splited.'.format(i + 1))
 print('All Done.')
--- preprocess_data/img_aug.py
+++ preprocess_data/img_aug.py
@@ -1,5 +1,7 @@
 import Augmentor
 import os
+from datetime import datetime
+
 def makedir(path):
     '''
     if path does not exist in the file system, create it
@@ -7,42 +9,72 @@ def makedir(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-datasets_root_dir = '/data/wangjiaqi/datasets/CUB_200_2011/CUB_200_2011/new_datasets/cub200_cropped/'
+def log_error(message, log_file="error_log.txt"):
+    '''
+    에러 발생 시 터미널 출력 및 로그 파일에 기록
+    '''
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    formatted_msg = f"[{timestamp}] {message}"
+    print(formatted_msg)
+    with open(log_file, "a") as f:
+        f.write(formatted_msg + "\n")
+
+# datasets_root_dir = '/data/wangjiaqi/datasets/CUB_200_2011/CUB_200_2011/new_datasets/cub200_cropped/'
+datasets_root_dir = '/nas/datasets/CUB_200_2011/cub200_cropped/'
 dir = datasets_root_dir + 'train_cropped/'
-target_dir = datasets_root_dir + 'train_cropped_augmented_2/'
+target_dir = datasets_root_dir + 'train_cropped_augmented/'
 
 makedir(target_dir)
 folders = [os.path.join(dir, folder) for folder in next(os.walk(dir))[1]]
 target_folders = [os.path.join(target_dir, folder) for folder in next(os.walk(dir))[1]]
 
+total_folders = len(folders)
+AUGMENT_FACTOR = 10
+
 for i in range(len(folders)):
     fd = folders[i]
     tfd = target_folders[i]
-    # rotation
-    p = Augmentor.Pipeline(source_directory=fd, output_directory=tfd)
-    p.rotate(probability=1, max_left_rotation=15, max_right_rotation=15)
-    p.flip_left_right(probability=0.5)
-    for i in range(10):
-        p.process()
-    del p
-    # skew
-    p = Augmentor.Pipeline(source_directory=fd, output_directory=tfd)
-    p.skew(probability=1, magnitude=0.2)  # max 45 degrees
-    p.flip_left_right(probability=0.5)
-    for i in range(10):
-        p.process()
-    del p
-    # shear
-    p = Augmentor.Pipeline(source_directory=fd, output_directory=tfd)
-    p.shear(probability=1, max_shear_left=10, max_shear_right=10)
-    p.flip_left_right(probability=0.5)
-    for i in range(10):
-        p.process()
-    del p
-    #random_distortion
-    p = Augmentor.Pipeline(source_directory=fd, output_directory=tfd)
-    p.random_distortion(probability=1.0, grid_width=10, grid_height=10, magnitude=5)
-    p.flip_left_right(probability=0.5)
-    for i in range(10):
-       p.process()
-    del p
+
+    folder_name = os.path.basename(fd)
+
+    try:
+        # 진행 상황 표시
+        print(f"[{i+1}/{total_folders}] Processing: {folder_name}")
+
+        valid_extensions = ('.jpg', '.jpeg', '.png', '.bmp', '.ppm')
+        files = [f for f in os.listdir(fd) if f.lower().endswith(valid_extensions)]
+        num_files = len(files)
+        if num_files == 0:
+            print(f"  -> [Skip] No images found in {folder_name}")
+            continue
+        num_samples = num_files * AUGMENT_FACTOR
+
+        # rotation
+        p = Augmentor.Pipeline(source_directory=fd, output_directory=tfd)
+        p.rotate(probability=1, max_left_rotation=15, max_right_rotation=15)
+        p.flip_left_right(probability=0.5)
+        p.sample(num_samples)
+
+        # (2) Skew
+        p = Augmentor.Pipeline(source_directory=fd, output_directory=tfd)
+        p.skew(probability=1, magnitude=0.2) 
+        p.flip_left_right(probability=0.5)
+        p.sample(num_samples)
+        
+        # (3) Shear
+        p = Augmentor.Pipeline(source_directory=fd, output_directory=tfd)
+        p.shear(probability=1, max_shear_left=10, max_shear_right=10)
+        p.flip_left_right(probability=0.5)
+        p.sample(num_samples)
+        
+        # (4) Random Distortion
+        p = Augmentor.Pipeline(source_directory=fd, output_directory=tfd)
+        p.random_distortion(probability=1.0, grid_width=10, grid_height=10, magnitude=5)
+        p.flip_left_right(probability=0.5)
+        p.sample(num_samples)
+
+    except Exception as e:
+        # 그 외 Augmentor 내부 에러나 알 수 없는 에러
+        log_error(f"Unexpected Error in '{folder_name}': {e}")
+        # 디버깅을 위해 상세 에러 내용 출력 (필요 시 주석 해제)
+        # traceback.print_exc()
